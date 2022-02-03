74a78290ee8447c028d870a9f9176d9bb421be00
CalsonAo <93045571+CalsonAo@users.noreply.github.com>
2022-02-04 03:08:55 +0800

    create empty project

---
+++ showcase-api/src/main/java/com/plymouth/comp2001/showcase/ShowcaseApiMain.java
@@ -0,0 +1,14 @@
+package com.plymouth.comp2001.showcase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShowcaseApiMain 
+{
+	private static final Logger logger = LoggerFactory.getLogger(ShowcaseApiMain.class);
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "init project.." );
+    }
+}
