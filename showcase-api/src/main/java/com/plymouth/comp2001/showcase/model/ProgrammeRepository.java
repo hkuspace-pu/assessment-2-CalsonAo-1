223a313d7ec78867d99b080c721f089783aac740
CalsonAo <93045571+CalsonAo@users.noreply.github.com>
2022-02-12 17:08:54 +0800

    apply store procedure call in repository

--- showcase-api/src/main/java/com/plymouth/comp2001/showcase/model/ProgrammeRepository.java
+++ showcase-api/src/main/java/com/plymouth/comp2001/showcase/model/ProgrammeRepository.java
@@ -3,8 +3,10 @@ package com.plymouth.comp2001.showcase.model;
 import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.StoredProcedureQuery;
 import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
@@ -23,27 +25,50 @@ public class ProgrammeRepository
 	
 	@Transactional
 	public int insert(Programme programme) {
-		Query query = em.createNativeQuery("insert into programme (title, create_time) values (:title, :create_time)");
-		query.setParameter("title", programme.getTitle());
-		query.setParameter("create_time", LocalDateTime.now());
+		
+		// native query
+//		Query query = em.createNativeQuery("insert into programme (title, create_time) values (:title, :create_time)");
+//		query.setParameter("title", programme.getTitle());
+//		query.setParameter("create_time", LocalDateTime.now());
+//		int result = query.executeUpdate();
+//		return result;
+		
+		// call store procedure
+		Query query = em.createNativeQuery("{call Create_Programme(?)}");           
+		query.setParameter(1, programme.getTitle());
 		int result = query.executeUpdate();
 		return result;
 	}
 	
 	@Transactional
 	public int update(Integer code, Programme programme) {
-		Query query = em.createNativeQuery("update programme set title=:title, last_update_time=:last_update_time where code = :code");
-		query.setParameter("code", code);
-		query.setParameter("title", programme.getTitle());
-		query.setParameter("last_update_time", LocalDateTime.now());
+		// native query
+//		Query query = em.createNativeQuery("update programme set title=:title, last_update_time=:last_update_time where code = :code");
+//		query.setParameter("code", code);
+//		query.setParameter("title", programme.getTitle());
+//		query.setParameter("last_update_time", LocalDateTime.now());
+//		int result = query.executeUpdate();
+//		return result;
+		
+		// call store procedure
+		Query query = em.createNativeQuery("{call Update_Programme(?, ?)}");           
+		query.setParameter(1, code);
+		query.setParameter(2, programme.getTitle());
 		int result = query.executeUpdate();
 		return result;
 	}
 	
 	@Transactional
 	public int delete(Integer code) {
-		Query query = em.createNativeQuery("delete from programme where code = :code");
-		query.setParameter("code", code);
+		// native query
+//		Query query = em.createNativeQuery("delete from programme where code = :code");
+//		query.setParameter("code", code);
+//		int result = query.executeUpdate();
+//		return result;
+		
+		// call store procedure
+		Query query = em.createNativeQuery("{call Delete_Programme(?)}");           
+		query.setParameter(1, code);
 		int result = query.executeUpdate();
 		return result;
 	}
